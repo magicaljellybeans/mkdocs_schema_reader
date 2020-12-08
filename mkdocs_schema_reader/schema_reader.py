241ebf1b6fdfb07b3b585887434f89661510ead0
Tom Robinson <thomas.robinson10@nhs.net>
2020-12-08 16:46:56 +0000

    ability to add json files and update nav

--- mkdocs_schema_reader/schema_reader.py
+++ mkdocs_schema_reader/schema_reader.py
@@ -3,7 +3,6 @@ import jsonschema2md
 import json
 
 from mkdocs.structure.files import File
-from mkdocs.config import config_options, Config
 from mkdocs.plugins import BasePlugin
 
 
@@ -12,20 +11,38 @@ class SchemaReader(BasePlugin):
     def on_files(self, files, config):
         parser = jsonschema2md.Parser()
         excludedirs = set(['venv', 'env', '__pycache__'])
+        schema_list = []
+        schema_dict = {"Schema": schema_list}
 
+        # Look for json files
         for root, dirs, filenames in os.walk('../'):
             dirs[:] = [d for d in dirs if d not in excludedirs]
+            
             for file in filenames:
                 if file.endswith('.json'):
+                    
                     with open(os.path.join(root, file)) as f:
+                        # Check file is a schema file
                         data = f.read()
                         schema_syntax = ["$schema", "$ref"]
+                        
                         if any(x in data for x in schema_syntax):
-                            # generate markdown file in site
-                            path = f"./site/schema/{file[:-5]}.md"
+                            # write converted markdown file to this location
+                            path = f"site/schema/{file[:-5]}.md"
+                            
                             with open(path, "w") as md:
                                 lines = parser.parse_schema(json.loads(data))
                                 for line in lines:
                                     md.write(line)
-                                files.append(File(path, config["docs_dir"], config["site_dir"], config["use_directory_urls"]))
+
+                                # Add to Files object 
+                                mkdocs_file = File(f"schema/{file[:-5]}.md", f"{os.getcwd()}/site", config["site_dir"], config["use_directory_urls"])
+                                files.append(mkdocs_file)
+
+                                # Add to schema list
+                                schema_list.append({f"{mkdocs_file.name}": f"{mkdocs_file.src_path}"})
+
+        # Add schemas to nav
+        config['nav'].append(schema_dict)
+
         return files
