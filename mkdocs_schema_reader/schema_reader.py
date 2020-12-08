887528539faa8641ec33fbd3a21b98151ba87229
Tom Robinson <thomas.robinson10@nhs.net>
2020-12-08 16:52:59 +0000

    linting

--- mkdocs_schema_reader/schema_reader.py
+++ mkdocs_schema_reader/schema_reader.py
@@ -7,42 +7,48 @@ from mkdocs.plugins import BasePlugin
 
 
 class SchemaReader(BasePlugin):
-
     def on_files(self, files, config):
         parser = jsonschema2md.Parser()
-        excludedirs = set(['venv', 'env', '__pycache__'])
+        excludedirs = set(["venv", "env", "__pycache__"])
         schema_list = []
         schema_dict = {"Schema": schema_list}
 
         # Look for json files
-        for root, dirs, filenames in os.walk('../'):
+        for root, dirs, filenames in os.walk("../"):
             dirs[:] = [d for d in dirs if d not in excludedirs]
-            
+
             for file in filenames:
-                if file.endswith('.json'):
-                    
+                if file.endswith(".json"):
+
                     with open(os.path.join(root, file)) as f:
                         # Check file is a schema file
                         data = f.read()
                         schema_syntax = ["$schema", "$ref"]
-                        
+
                         if any(x in data for x in schema_syntax):
                             # write converted markdown file to this location
                             path = f"site/schema/{file[:-5]}.md"
-                            
+
                             with open(path, "w") as md:
                                 lines = parser.parse_schema(json.loads(data))
                                 for line in lines:
                                     md.write(line)
 
-                                # Add to Files object 
-                                mkdocs_file = File(f"schema/{file[:-5]}.md", f"{os.getcwd()}/site", config["site_dir"], config["use_directory_urls"])
-                                files.append(mkdocs_file)
+                            # Add to Files object
+                            mkdfile = File(
+                                f"schema/{file[:-5]}.md",
+                                f"{os.getcwd()}/site",
+                                config["site_dir"],
+                                config["use_directory_urls"],
+                            )
+                            files.append(mkdfile)
 
-                                # Add to schema list
-                                schema_list.append({f"{mkdocs_file.name}": f"{mkdocs_file.src_path}"})
+                            # Add to schema list
+                            schema_list.append(
+                                {f"{mkdfile.name}": f"{mkdfile.src_path}"}
+                            )
 
         # Add schemas to nav
-        config['nav'].append(schema_dict)
+        config["nav"].append(schema_dict)
 
         return files
--- setup.py
+++ setup.py
@@ -2,24 +2,21 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='mkdocs_schema_reader',
-    version='0.1.0',
-    description='A MkDocs plugin to collate json schema files and convert them into markdown files',
-    long_description='',
-    keywords='mkdocs, schema, json',
-    url='',
-    author='Tom Robinson',
-    author_email='tome.robin@gail.com',
-    license='MIT',
-    python_requires='>=2.7',
-    install_requires=[
-        'mkdocs>=1.0.4',
-        'jsonschema2md'
-    ],
+    name="mkdocs_schema_reader",
+    version="0.1.0",
+    description="A MkDocs plugin to collate json schema files and convert them into markdown files",
+    long_description="",
+    keywords="mkdocs, schema, json",
+    url="",
+    author="Tom Robinson",
+    author_email="tome.robin@gail.com",
+    license="MIT",
+    python_requires=">=2.7",
+    install_requires=["mkdocs>=1.0.4", "jsonschema2md"],
     packages=find_packages(),
     entry_points={
-        'mkdocs.plugins': [
-            'schema_reader = mkdocs_schema_reader.schema_reader:SchemaReader'
+        "mkdocs.plugins": [
+            "schema_reader = mkdocs_schema_reader.schema_reader:SchemaReader"
         ]
-    }
+    },
 )
